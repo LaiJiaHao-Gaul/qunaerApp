860e15e9e4eabcdda06fc0cbe560334159de0bb0
LaiGaHao <787071638@qq.com>
2020-01-22 14:22:43 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='wrapper'>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for='item of swiperList' :key='item.id' >
+        <img class='swiper-img' :src='item.imgUrl' />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
+  .wrapper
+    width: 100%
+    height :0
+    overflow:hidden
+    padding-bottom :37%
+    background :#eee
+    .swiper-img
+      width:100%
+</style>
