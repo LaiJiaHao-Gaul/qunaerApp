2bb090dc47d832bf67cee1bd7490a8bb83a51ca5
nunu <787071638@qq.com>
2021-01-08 18:19:58 +0800

    city-router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
