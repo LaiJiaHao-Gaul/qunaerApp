7bff1f3a1893bee37698626a8e79c9c24472e751
nunu <787071638@qq.com>
2021-01-11 15:30:55 +0800

    list finish

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -21,13 +21,45 @@ export default {
   data () {
     return {
       list: [{
-        title: '成人票'
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
       }, {
-        title: '学生票'
+        title: '学生票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
       }, {
-        title: '儿童票'
+        title: '儿童票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
       }, {
-        title: '特惠票'
+        title: '特惠票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
       }]
     }
   }
