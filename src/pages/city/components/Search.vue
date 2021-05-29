5b04a1f19ea978dcdd19ff14c899e6f73eb2c404
nunu <787071638@qq.com>
2021-05-29 16:27:52 +0800

    add Bscroll

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -63,7 +63,9 @@ export default {
     }
   },
   mounted () {
-    this.scroll = new Bscroll(this.$refs.search)
+    this.scroll = new Bscroll(this.$refs.search, {
+      click: true
+    })
   }
 }
 </script>
